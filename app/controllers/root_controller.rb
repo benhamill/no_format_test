e476606d86495325bdb98161153fcc5e914638b2
Ben Hamill <git-commits@benhamill.com>
2013-10-23 00:12:02 -0500

    This works, I guess?

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,5 @@
 class RootController < ApplicationController
   def index
+    Rails.logger.error params[:bar]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 NoFormatTest::Application.routes.draw do
   root 'root#index'
-  get '/foo/:bar/baz', to: 'root#index', format: false
+  scope format: false do
+    scope '/foo/:bar' do
+      constraints bar: %r{[^/]+} do
+        get '/', to: 'root#index'
+        get '/baz', to: 'root#index'
+      end
+    end
+  end
 end
